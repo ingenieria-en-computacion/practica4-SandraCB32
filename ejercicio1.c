dec5af1d56cd4ccd5e6f0021fa46713af85b325b
SandraCB32 <sandracb365@gmail.com>
2025-03-06 09:52:29 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int main(){
+    int *arreglo, num, cont;
+    printf("¿Cuántos elementos tiene el conjunto?\n");
+    scanf("%d", &num);
+    arreglo = (int*)malloc(num *sizeof(int));
+    if(arreglo != NULL)
+    { //lo usamos el nulo para saber si malloc si lo pudo guardar
+        printf("Vector reservado:\n\t[");
+        for(cont =0; cont <num; cont++){
+            printf("\t%d, %p", *(arreglo + cont), (arreglo + cont));            
+        }
+        printf("\t]\n");
+        printf("Liberando el espacio reservado\n");
+        free(arreglo);
+        arreglo = NULL;       
+    }
+    return 0;
+}
